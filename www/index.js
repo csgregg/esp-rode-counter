69faf2a7b853d4ee75385bcbf5e05c3ac4459346
csgregg <csgregg@users.noreply.github.com>
2023-03-24 22:25:47 +0000

    first functioning UI

--- www/index.js
+++ www/index.js
@@ -1,9 +1,19 @@
+var chain_up = 'f';
+var chain_down = 'f';
+var current_rode = 0;
+var water_line = 0;
+var rode_len = 0;
+var warn_limit_1 = 0;
+var warn_limit_2 = 0;
+var warn_limit_3 = 0;
+
+
 // https://github.com/naikus/svg-gauge
-var gauge2 = Gauge(
-    document.getElementById("gauge2"),
+var rode_gauge = Gauge(
+    document.getElementById("rode_gauge"),
     {
       min: 0,
-      max: 50.00,
+      max: 100,
       dialStartAngle:360,
       dialEndAngle:90,
       viewBox: "10 40 100 60",
@@ -11,13 +21,13 @@ var gauge2 = Gauge(
         return value.toFixed(2) + "m";
       },
       color: function(value) {
-        if(value < 2.00) {
+        if(value <= water_line/100 ) {
           return "Tomato";
-        }else if(value < 10.00) {
+        }else if(value <= warn_limit_1/100) {
           return "Orange";
-        }else if(value < 45.00) {
+        }else if(value < warn_limit_2/100) {
           return "MediumSeaGreen";
-        }else if(value < 48.00) {
+        }else if(value < warn_limit_3/100) {
             return "Orange";
         }else {
           return "Tomato";
@@ -33,7 +43,24 @@ function initPage() {
 }
 
 function updatePage() {
-    // console.log("Status - Update Page");
-          // Set gauge value
-          gauge2.setValue(20.1);
+    //console.log("Status - Update Page");
+
+    // Set gauge value and max in m
+    var x = current_rode / 100;
+    if( current_rode != 0 ) rode_gauge.setValue( x.toFixed(2));
+    if( rode_len != 0 ) rode_gauge.setMaxValue(rode_len/100);
+
+    // Set chain direction
+    if( chain_up == 't' ) document.getElementById("chain").style.flexDirection = 'column-reverse';
+    if( chain_down == 't' ) document.getElementById("chain").style.flexDirection = 'column';
+
+    // Animate chain
+    const collection = document.getElementsByClassName("chain__element");
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.animationPlayState = ( chain_up == 't' || chain_down == 't' ) ? 'running' : 'paused';
+    }
+    
+    x = rode_len / 100;
+    var y = water_line / 100;
+    document.getElementById("chain_spec").innerHTML = "Chain Length: " + x.toFixed(2) + "m / Water Line: " + y.toFixed(2) + "m";
 }
